dfa0a556f99c91132df0e8a22e8734c4870ebbf3
shobhamrj <shobham.rajak@gmail.com>
2020-09-05 15:08:45 +0530

    merging 2nd time

---
+++ Math/src/SeiveOfEratosthenes.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class SeiveOfEratosthenes {
+
+    public static void main(String[] args) {
+        seive(200);
+    }
+
+    private static void seive(int n) {
+        if(n <= 1)
+            return;
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+        for(int i = 2; i * i <= n; i++) {
+            if(isPrime[i]) {
+                for(int j = 2*i; j <= n; j = j+i)
+                    isPrime[j] = false;
+            }
+        }
+        for(int i = 2; i <= n; i++) {
+            if(isPrime[i])
+                System.out.print(i + " ");
+        }
+    }
+}
