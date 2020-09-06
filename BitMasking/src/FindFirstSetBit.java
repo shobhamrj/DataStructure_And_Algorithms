884851500a1b928f06a0370bb816c0a548b9c160
shobhamrj <shobham.rajak@gmail.com>
2020-09-06 17:19:08 +0530

    first set bit

--- BitMasking/src/FindFirstSetBit.java
+++ BitMasking/src/FindFirstSetBit.java
@@ -1,10 +1,19 @@
 public class FindFirstSetBit {
 
     public static void main(String[] args) {
-   //     System.out.println(isSet(7));
+        System.out.println(isSet(64));
     }
 
-   // private static boolean isSet(int n) { }
+    private static int isSet(int n) {
+        int count = 1;
+        while (n > 0) {
+            if ((n & 1) != 0)
+                return count;
+            n >>= 1;
+            count++;
+        }
+        return count;
+    }
 
 
 }
