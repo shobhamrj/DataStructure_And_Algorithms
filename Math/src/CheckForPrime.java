b1ba76a30f709bb756ae6976c6e06bfcbbbc5cfc
Shobhm Rajak <shobham.rajak@gmail.com>
2020-09-12 00:53:39 +0530

    last algo for mathematics

--- Math/src/CheckForPrime.java
+++ Math/src/CheckForPrime.java
@@ -4,8 +4,8 @@ public class CheckForPrime {
         System.out.println(isPrime(49));
     }
 
-    private static boolean isPrime (int n) {
-        if(n == 1)
+    public static boolean isPrime (int n) {
+        if(n <= 1)
             return false;
         if(n == 2 || n == 3)
             return true;
---
+++ Math/src/Exactly3Divisors.java
@@ -0,0 +1,16 @@
+public class Exactly3Divisors {
+
+    public static void main(String[] args) {
+        System.out.println(cal(6));
+    }
+
+    private static int cal(int n) {
+        int count = 0;
+        n = (int)Math.sqrt(n);
+        for(int i = 2 ; i <= n; i++) {
+            if(CheckForPrime.isPrime(i))
+                count++;
+        }
+        return count;
+    }
+}  //O( N^(1/2) * N^(1/4) )
