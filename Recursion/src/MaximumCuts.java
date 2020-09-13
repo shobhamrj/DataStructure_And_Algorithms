43d3b570bd24052ff45240185976367622fb8656
Shobhm Rajak <shobham.rajak@gmail.com>
2020-09-13 16:48:31 +0530

    Max cut and subset added

--- Recursion/src/MaximumCuts.java
+++ Recursion/src/MaximumCuts.java
@@ -7,13 +7,10 @@ public class MaximumCuts {
     }
 
     private static int maxCut(int n, int a, int b, int c) {
-        if(n == 0)
-            return 0;
-        if(n < 0)
-            return -1;
+        if(n == 0) return 0;
+        if(n < 0) return -1;
         int result = Math.max( (Math.max(maxCut(n - a, a, b, c), maxCut(n - b, a, b, c))), maxCut(n - c, a, b, c) );
-        if(result == -1)
-            return -1;
+        if(result == -1) return -1;
         return result + 1;
     }
 }
--- Recursion/src/SubSets.java
+++ Recursion/src/SubSets.java
@@ -6,10 +6,11 @@ public class SubSets {
 
     private static void findSubset(String str, String curr , int index ) {
         if(index == str.length()) {
-            System.out.print(curr);
+            System.out.print(curr.toString());
             return;
         }
         findSubset(str, curr, index + 1);
         findSubset(str, curr + str.charAt(index), index + 1);
     }
-}
+}//Time Complexity : O(2^n)
+
