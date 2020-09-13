9241a96aab1da87f994bcb82127e96724b0f585d
Shobhm Rajak <shobham.rajak@gmail.com>
2020-09-13 23:47:34 +0530

    tower of hanoi added

---
+++ Recursion/src/TowerOfHanoi.java
@@ -0,0 +1,16 @@
+public class TowerOfHanoi {
+
+    public static void main(String[] args) {
+        toh(10, 'A', 'B', 'C');
+    }
+
+    private static void toh(int n, char a, char b, char c) {
+        if(n == 1) {
+            System.out.println("Move 1 from " + a + " to " + c);
+            return;
+        }
+        toh(n - 1, a, c, b);
+        System.out.println("Move " + n +" from " + a + " to " + c);
+        toh(n - 1, b, a, c);
+    }
+}
