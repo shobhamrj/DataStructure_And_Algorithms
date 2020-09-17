3eceef21c00f82b8a6e2a9bb5b2567483ea37aa5
Shobhm Rajak <shobham.rajak@gmail.com>
2020-09-17 22:20:56 +0530

    started arrays

--- Recursion/src/PossibleWordsFromDialler.java
+++ Recursion/src/PossibleWordsFromDialler.java
@@ -15,19 +15,18 @@ public class PossibleWordsFromDialler {
         put("8", "tuv");
         put("9", "wxyz");
     }};
+    private static List<String> output = new ArrayList<>();
 
-    private static List<String> output = new ArrayList<String>();
-
-    public static void backtrack(String combination, String next_digits) {
-        if (next_digits.length() == 0) {
+    public static void backtrack(String combination, String nextDigits) {
+        if (nextDigits.length() == 0) {
             output.add(combination);
         }
         else {
-            String digit = next_digits.substring(0, 1);
+            String digit = nextDigits.substring(0, 1);
             String letters = phone.get(digit);
             for (int i = 0; i < letters.length(); i++) {
                 String letter = phone.get(digit).substring(i, i + 1);
-                backtrack(combination + letter, next_digits.substring(1));
+                backtrack(combination + letter, nextDigits.substring(1));
             }
         }
     }
