d2cc7a2c0a72f684e9b6d3b4d8c8537430885c8a
Shobhm Rajak <shobham.rajak@gmail.com>
2020-09-17 22:14:18 +0530

    PossibleWordsFromDialler added

---
+++ Recursion/src/PossibleWordsFromDialler.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PossibleWordsFromDialler {
+
+    private static Map<String, String> phone = new HashMap<String, String>() {{
+        put("2", "abc");
+        put("3", "def");
+        put("4", "ghi");
+        put("5", "jkl");
+        put("6", "mno");
+        put("7", "pqrs");
+        put("8", "tuv");
+        put("9", "wxyz");
+    }};
+
+    private static List<String> output = new ArrayList<String>();
+
+    public static void backtrack(String combination, String next_digits) {
+        if (next_digits.length() == 0) {
+            output.add(combination);
+        }
+        else {
+            String digit = next_digits.substring(0, 1);
+            String letters = phone.get(digit);
+            for (int i = 0; i < letters.length(); i++) {
+                String letter = phone.get(digit).substring(i, i + 1);
+                backtrack(combination + letter, next_digits.substring(1));
+            }
+        }
+    }
+
+    public static List<String> letterCombinations(String digits) {
+        if (digits.length() != 0)
+            backtrack("", digits);
+        return output;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(letterCombinations("2345"));
+    }
+
+}
+
+/*
+  For reference see,
+
+  1 -->
+  2 --> a,b,c
+  3 --> d,e,f
+  4 --> g,h,i
+  5 --> j,k,l
+  6 --> m,n,o
+  7 --> p,q,r,s
+  8 --> t,u,v
+  9 --> w,x,y,z
+ */
