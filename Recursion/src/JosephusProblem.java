ec2e7331566fcf5f20cde4698bf39a26d72ae929
Shobhm Rajak <shobham.rajak@gmail.com>
2020-09-17 15:57:34 +0530

    josephus problem

--- Recursion/src/JosephusProblem.java
+++ Recursion/src/JosephusProblem.java
@@ -5,6 +5,10 @@ public class JosephusProblem {
     }
 
     private static int jos(int n, int k) {
-        return n == 1 ? 0 : (jos(n - 1, k) + k) % n;
+        return n == 1 ? 0 : (jos(n - 1, k) + k) % n ;
     }
 }
+// T(n) = T(n - 1) + c
+// Time Complexity : O(n)
+// indices of people in a circle are considered from 0 here
+// for more read the Josephu's problem statement
